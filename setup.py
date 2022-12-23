64bee1cf06f026fcb20d9afa8ac3930c8a2f1e41
evening <39542044+evening@users.noreply.github.com>
2022-12-23 15:41:45 -0500

    bump ver

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="urbitob",
-    version="0.0.3",
+    version="0.0.4",
     author="~ranren-ranlen",
     description="A python implementation of urbit-ob",
     long_description=long_description,
